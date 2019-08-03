ddeb3e770771ad51649a9d377152207df3d36727
Loic Trihan <lolotrihan@hotmail.fr>
2019-08-03 12:53:50 +0200

    add files for homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
-
-# Cocktail.create(name: "Mojito")
-# Cocktail.create(name: "Gromito")
-# Cocktail.create(name: "SexOnTheBeach")
 require 'json'
 require 'open-uri'
 
