e6cd0d1cb13754c45ab6ec5b073836e7799a69ee
Loic Trihan <lolotrihan@hotmail.fr>
2019-08-05 01:06:41 +0200

    begin of cocktails show & new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,22 @@ Cocktail.destroy_all
 Dose.destroy_all
 Ingredient.destroy_all
 
-# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# ingredient = open(url).read
-# user = JSON.parse(ingredient)
-
-# user['drinks'].each do |drink|
-#   ingredient = Ingredient.create!(name: drink['strIngredient1'])
-#   cocktail = Cocktail.create!(name: Faker::Beer.name)
-#   Dose.create!(
-#     description: ['1cl', '2cl', '3cl'].sample,
-#     ingredient: ingredient, cocktail: cocktail
-#   )
-# end
-
 Cocktail.create!(name: 'Ananas', photo: 'ananas.jpg')
 Cocktail.create!(name: 'Ice Tea Lemon', photo: 'icetealemon.jpg')
 Cocktail.create!(name: 'Mûre Subite', photo: 'muresubite.jpg')
 Cocktail.create!(name: 'Orange', photo: 'orange.jpg')
 Cocktail.create!(name: 'Sugar Fever', photo: 'sugarfever.jpg')
 Cocktail.create!(name: 'Cocktails partagés', photo: 'sharedcocktail.jpg')
+
+ingredients = ['Coca', "Jus d'orange", 'Jus de pomme', 'Orangina', 'Schweppes', 'Rhum brun', 'Rhum blanc', 'Whiskey', 'Vodka', 'Eau']
+ingredients.each { |ingredient| Ingredient.create!(name: ingredient) }
+
+Cocktail.all.each do |cocktail|
+  # cocktail = Cocktail.create!(name: Faker::Beer.name)
+  3.times do
+    Dose.create!(
+      description: ['1cl', '2cl', '3cl'].sample,
+      ingredient: Ingredient.all.sample, cocktail: cocktail
+    )
+  end
+end
